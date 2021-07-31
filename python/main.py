d95eb0cb4ad669a66485669b971927cfc1abdf26
benwade91 <70988204+benn925@users.noreply.github.com>
2021-07-31 06:39:55 -0700

    game functions properly. no frills!

--- python/main.py
+++ python/main.py
@@ -5,8 +5,12 @@ print(art.guess)
 print(art.the)
 print(art.number)
 print("I'm thinking of a number between 1 and 100")
+
+number = random.randint(1,100)
 turns = 0
+
 difficulty = input("Chose a difficulty. 'easy' or 'hard'?\n")
+
 if difficulty == 'easy':
     turns +=10
 else:
@@ -15,5 +19,20 @@ else:
 gameover = False
 
 while not gameover:
-    print(turns)
-    gameover = True
+    print(f"You've got {turns} turns left!")
+    guess = int(input("Guess a number!\n"))
+
+    if guess > number:
+        print("too high!")
+        turns -= 1
+    elif guess < number:
+        print("too low!")
+        turns -= 1
+    elif guess == number:
+        print("Thats it! You Win!")
+        gameover = True
+
+    if turns == 0:
+        print("You used all your chances!")
+        print("GAME OVER")
+        gameover = True
